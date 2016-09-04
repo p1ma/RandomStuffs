263183f8b88ae1e2fc30dd93fa4738f11b2fe0f4
p1ma <pierre.junges@orange.fr>
2016-09-04 10:34:22 +0200

    fix arg problem

--- CPP/optimization.cpp
+++ CPP/optimization.cpp
@@ -6,6 +6,10 @@ using namespace std;
 Optimize *pOpti;
 
 int main(int argc, char **argv){
+  if(argc != 2){
+    std::cerr << "BAD USAGE : ./optimization <file.csv>" << std::endl;
+    exit(EXIT_FAILURE);
+  }
   cout << "\tTest optimization" << endl;
   pOpti = new Optimize(argv[1]);
   cout << pOpti->printMax() << std::endl;
